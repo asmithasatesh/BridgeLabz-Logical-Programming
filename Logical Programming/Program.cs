321772e8119649954191e59d6ea86a77749b6a98
asmithasatesh <asmithasatesh@gmail.com>
2021-07-01 11:51:04 +0530

    [Asmitha]Adding 2.Stopwatch Simulate Program

--- Logical Programming/Program.cs
+++ Logical Programming/Program.cs
@@ -8,12 +8,15 @@ namespace Logical_Programming
         {
 
             Console.WriteLine("Enter the which program to execute");
-            Console.WriteLine("1.CouponbNumbers");
+            Console.WriteLine("1.CouponbNumbers 2.Stopwatch Simulate");
             int ch = Convert.ToInt32(Console.ReadLine());
             switch (ch)
             {
                 case 1:
                     Coupon_Numbers.ReadInput();
+                    break;
+                case 2:
+                    Stopwatch_Simulate.ReadInput();
                     break;
             }
         }
