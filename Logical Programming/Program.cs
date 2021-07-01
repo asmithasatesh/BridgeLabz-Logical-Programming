bc8e09c2912cd1efde9497bcb5fbcdac0a531cd6
asmithasatesh <asmithasatesh@gmail.com>
2021-07-01 11:45:10 +0530

    [Asmitha]Adding 1.Coupon Numbers Program

--- Logical Programming/Program.cs
+++ Logical Programming/Program.cs
@@ -6,7 +6,16 @@ namespace Logical_Programming
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Enter the which program to execute");
+            Console.WriteLine("1.CouponbNumbers");
+            int ch = Convert.ToInt32(Console.ReadLine());
+            switch (ch)
+            {
+                case 1:
+                    Coupon_Numbers.ReadInput();
+                    break;
+            }
         }
     }
 }
